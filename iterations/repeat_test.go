7e2313cfe18529550a1cc26c04a49ef29b82acb8
mirza-sync <mirzasyahmi123@gmail.com>
2025-04-24 22:45:32 +0800

    Chapter 4: Iterations

---
+++ iterations/repeat_test.go
@@ -0,0 +1,12 @@
+package iterations
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expect %q got %q", expected, repeated)
+	}
+}
