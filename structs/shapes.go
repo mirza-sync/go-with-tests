6e850174554e94a6850955b438933127c61a310b
mirza-sync <mirzasyahmi123@gmail.com>
2025-04-28 23:05:49 +0800

    Add Triangle struct

--- structs/shapes.go
+++ structs/shapes.go
@@ -23,6 +23,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+type Triangle struct {
+	Height float64
+	Base   float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Height * t.Base) * 0.5
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -19,11 +19,12 @@ func TestArea(t *testing.T) {
 	}{
 		{Rectangle{12.0, 6.0}, 72.0},
 		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 	for _, tt := range areaTests {
 		got := tt.shape.Area()
 		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
 		}
 	}
 }
