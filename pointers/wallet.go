278574769d9daf9de20a1962def67eefadceca40
mirza-sync <mirzasyahmi123@gmail.com>
2025-05-07 23:47:50 +0800

    Add custom type 'Bitcon'

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -2,15 +2,17 @@ package pointers
 
 import "fmt"
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	return w.balance
 }
