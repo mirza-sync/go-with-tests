e8938a6851bae291e4603ba4153e1ae52e2e2925
mirza-sync <mirzasyahmi123@gmail.com>
2025-05-08 00:04:54 +0800

    Add withdraw method

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -6,15 +6,30 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
-	want := Bitcoin(10)
+		got := wallet.Balance()
+		fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
