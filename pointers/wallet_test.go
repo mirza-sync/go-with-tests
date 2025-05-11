3eba8621eae3c42b2dd3f75ce53c1ae39fcdc90b
mirza-sync <mirzasyahmi123@gmail.com>
2025-05-11 23:56:57 +0800

    Add assertError function

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -13,16 +13,20 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
+
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
 		startingBalance := Bitcoin(20)
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
 
+		assertError(t, err)
 		assertBalance(t, wallet, startingBalance)
-
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
 	})
 
 	t.Run("deposit", func(t *testing.T) {
