1ba81301e653125e86c1fde1079013c8c711cbc7
mirza-sync <mirzasyahmi123@gmail.com>
2025-05-11 23:41:41 +0800

    Refactor test

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -13,11 +13,11 @@ type Wallet struct {
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
-	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	return w.balance
 }
 
