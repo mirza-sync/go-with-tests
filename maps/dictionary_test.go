ca12d8e407d09ae9a16005f61912796af2995d95
mirza-sync <mirzasyahmi123@gmail.com>
2025-05-13 23:40:51 +0800

    Add new map type

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,12 +3,18 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
+	got := dictionary.Search("test")
 	want := "this is just a test"
 
+	assertString(t, got, want)
+}
+
+func assertString(t testing.TB, got string, want string) {
+	t.Helper()
+
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
