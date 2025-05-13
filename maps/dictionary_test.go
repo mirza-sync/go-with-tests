00e2f1572ecb5fa4ff1036a77eac18fbdda6829b
mirza-sync <mirzasyahmi123@gmail.com>
2025-05-13 23:51:41 +0800

    Add test case for unknown word search

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("know word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertString(t, got, want)
+		assertString(t, got, want)
+	})
+
+	t.Run("unknow word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertString(t, err.Error(), want)
+	})
 }
 
 func assertString(t testing.TB, got string, want string) {
