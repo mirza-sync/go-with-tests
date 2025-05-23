45bb0cb2db9661310e423f4ac1ac38d6d6958777
mirza-sync <mirzasyahmi123@gmail.com>
2025-05-23 23:54:53 +0800

    Add update method

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -46,6 +46,17 @@ func TestAdd(t *testing.T) {
 	})
 }
 
+func TestUpdate(t *testing.T) {
+	word := "test"
+	definition := "this is just a test"
+	dictionary := Dictionary{word: definition}
+	newDefinition := "new definition"
+
+	dictionary.Update(word, newDefinition)
+
+	assertDefinition(t, dictionary, word, newDefinition)
+}
+
 func assertString(t testing.TB, got string, want string) {
 	t.Helper()
 
