42319ae46ce2874e2e6d9c02402e9108125545b1
mirza-sync <mirzasyahmi123@gmail.com>
2025-05-26 22:41:24 +0800

    Check word exist or not before deleting

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -71,13 +71,24 @@ func TestUpdate(t *testing.T) {
 }
 
 func TestDelete(t *testing.T) {
-	word := "test"
-	dictionary := Dictionary{word: "test definition"}
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{word: "test definition"}
 
-	dictionary.Delete(word)
+		err := dictionary.Delete(word)
+		assertError(t, err, nil)
 
-	_, err := dictionary.Search(word)
-	assertError(t, err, ErrNotFound)
+		_, err = dictionary.Search(word)
+		assertError(t, err, ErrNotFound)
+	})
+
+	t.Run("non-existing word", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Delete(word)
+		assertError(t, err, ErrWordDoesNotExist)
+	})
 }
 
 func assertString(t testing.TB, got string, want string) {
