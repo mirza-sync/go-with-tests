df32aff4453f88a72e27b7fad6182bc76b87f673
mirza-sync <mirzasyahmi123@gmail.com>
2025-05-31 23:18:16 +0800

    Add test: sleep before every print

--- mocking/mock.go
+++ mocking/mock.go
@@ -9,6 +9,8 @@ import (
 
 const finalWord = "Go!"
 const countdownStart = 3
+const sleep = "sleep"
+const write = "write"
 
 type Sleeper interface {
 	Sleep()
@@ -28,11 +30,25 @@ func (d DefaultSleeper) Sleep() {
 
 type DefaultSleeper struct{}
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
 
